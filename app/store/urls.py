788f47f3243db38130ab5235e4760212bf8c9e84
Matheus Eidt <matheus@mx2.unisc.br>
2022-04-18 18:16:13 -0300

    feat: creating new endpoint to deactivate/activate product from the model (WIP).

--- app/store/urls.py
+++ app/store/urls.py
@@ -5,5 +5,6 @@ from .views import ListProducts
 app_name = "store"
 
 urlpatterns = [
-    path("products", ListProducts.as_view()),
+    path("products/", ListProducts.as_view()),
+    path("products/<int:pk>", ListProducts.as_view()),
 ]
--- app/store/views.py
+++ app/store/views.py
@@ -1,5 +1,23 @@
 from rest_framework.views import APIView
+from app.store.models import Products
+from app.store.serializers import ProductsSerializer
+from django.shortcuts import get_object_or_404
+from rest_framework.response import Response
+from rest_framework import status
 
 
 class ListProducts(APIView):
-    pass
+    serializer_class = ProductsSerializer
+    model = Products
+
+    def get(self, request):
+        serializer = self.serializer_class(self.model.objects.all())
+        return Response(data=serializer.data)
+
+    def patch(self, request, pk):
+        instance = get_object_or_404(self.model, pk=pk)
+        serializer = self.serializer_class(instance)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(data=serializer.data)
+        return Response(status=status.HTTP_400_BAD_REQUEST, data=serializer.errors)
